54e4d42f407d4d491f38c97fc8171271298fa5ed
Jefferson Arriaza <95657230+JeffersonArriaza@users.noreply.github.com>
2024-08-16 21:01:37 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "lib/server_functions.h"
+
+#define PORT 5050
+
+int main() {
+
+    int server_fd, client_fd;
+    server_fd = srv_init(PORT);
+
+    while (1) {
+        
+        client_fd = srv_accept_client(server_fd);
+
+        if (client_fd < 0) {
+            continue;
+        }
+    sleep(3); //Espera 3 segundos para no cerrar la conexion inmediatamente
+    close(client_fd);
+    }
+
+ close(server_fd);
+ return 0;
+} 
